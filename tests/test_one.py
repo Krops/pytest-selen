9fc4e577863105c08fe2bac6f7a6e338b8726680
Andrii Kropyva <akropyva.ctr@virtru.com>
2019-01-14 19:58:04 +0200

    Add api tests

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,16 +1,72 @@
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
+import requests as req
+from jsonschema import validate
+import json
 
 
-def test_facebook_on():
-    capabilities = FirefoxOptions()
-    desired_caps = capabilities.to_capabilities()
-    desired_caps['enableVNC'] = True
-    desired_caps['enableVideo'] = True
+# def test_facebook_on():
+#    capabilities = FirefoxOptions()
+#    desired_caps = capabilities.to_capabilities()
+#    desired_caps['enableVNC'] = True
+#    desired_caps['enableVideo'] = True
 
-    driver = webdriver.Remote(
-        command_executor="http://selenoid:4444/wd/hub",
-        desired_capabilities=desired_caps)
+#    driver = webdriver.Remote(
+#        command_executor="http://selenoid:4444/wd/hub",
+#        desired_capabilities=desired_caps)
 
-    driver.get("https://www.facebook.com")
-    driver.quit()
+#    driver.get("https://www.facebook.com")
+#    driver.quit()
+
+server_name = "https://reqres.in"
+
+def test_two():
+    validate_get_json_request("/api/users?page=2", "test_two_schema")
+
+
+def test_three():
+    validate_post_json_request("/api/users", "test_three_schema", "test_three_data")
+
+
+def test_four():
+    validate_get_json_request("/api/users", "test_four_schema")
+
+def test_five():
+    validate_delete_request("/api/users/2")
+
+
+def test_six():
+    validate_get_request("/api/users/23", 404)
+
+
+def test_seven():
+    validate_get_json_request("/api/users", "test_seven_schema")
+
+
+def validate_get_json_request(api, schema_name):
+    resp = req.get(server_name + api)
+    assert resp.status_code == 200
+    with open('resources/schemas/{}.json'.format(schema_name), 'r') as f:
+        schema = json.load(f)
+    validate(resp.json(), schema=schema)
+
+
+def validate_post_json_request(api, schema_name, post_data):
+    with open('resources/test_data/{}.json'.format(post_data), 'r') as f:
+        data = json.load(f)
+    resp = req.post(server_name + api, data)
+    assert resp.status_code == 201
+    with open('resources/schemas/{}.json'.format(schema_name), 'r') as f:
+        schema = json.load(f)
+    validate(resp.json(), schema=schema)
+
+
+def validate_delete_request(api):
+    resp = req.delete(server_name + api)
+    assert resp.status_code == 204
+    assert resp.text == ""
+
+
+def validate_get_request(api, status_code):
+    resp = req.get(server_name + api)
+    assert resp.status_code == status_code
